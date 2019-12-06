b49c5f2a37b123bb02eec9d558ee538a999113ee
DHIII <dh3code@gmail.com>
2019-12-06 07:14:11 -0500

    Added event loop

---
+++ program.py
@@ -0,0 +1,42 @@
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('-------------------------')
+    print('     JOURNAL APP')
+    print('-------------------------')
+    print()
+
+
+def run_event_loop():
+    print('What do you want to do with your journal?')
+
+    cmd = None
+
+    while cmd != 'x':
+
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+
+        cmd = cmd.upper().strip()
+
+        if cmd == 'L':
+            list_entries()
+        elif cmd == 'A':
+            add_entry()
+        elif cmd != 'X':
+            print("Sorry, we don't understand '{}'.".format(cmd))
+
+    print('Done, goodbye')
+
+
+def list_entries():
+    print('Listing...')
+
+
+def add_entry():
+    print('Adding entry...')
+
+
+main()
