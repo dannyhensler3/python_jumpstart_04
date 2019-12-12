8d95f2f699fe1c9c094aa93d84dc9dc95fee44e9
DHIII <dh3code@gmail.com>
2019-12-11 22:45:45 -0500

    finsihed journal module

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+import journal
+
+
 def main():
     print_header()
     run_event_loop()
@@ -12,24 +15,23 @@ def print_header():
 
 def run_event_loop():
     print('What do you want to do with your journal?')
+    cmd = 'EMPTY'
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)  # []  # list()
 
-    cmd = None
-    journal_data = []
-
-    while cmd != 'x':
-
+    while cmd != 'x' and cmd:
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+        cmd = cmd.lower().strip()
 
-        cmd = cmd.upper().strip()
-
-        if cmd == 'L':
+        if cmd == 'l':
             list_entries(journal_data)
-        elif cmd == 'A':
+        elif cmd == 'a':
             add_entry(journal_data)
-        elif cmd != 'X':
+        elif cmd != 'x' and cmd:
             print("Sorry, we don't understand '{}'.".format(cmd))
 
-    print('Done, goodbye')
+    print('Done, goodbye.')
+    journal.save(journal_name, journal_data)
 
 
 def list_entries(data):
@@ -41,7 +43,11 @@ def list_entries(data):
 
 def add_entry(data):
     text = input('Type your entry, <enter> to exit: ')
-    data.append(text)
+    journal.add_entry(text, data)
 
 
-main()
+print(__file__)
+print(__name__)
+
+if __name__ == '__main__':
+    main()
